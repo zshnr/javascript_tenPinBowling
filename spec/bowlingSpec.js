3b506414cebd92358c320b27b258d7800405d429
Zeeshan Rasool <zrasool88@gmail.com>
2014-10-18 23:27:46 +0100

    wrote test for game to have 10 frames at start

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -4,4 +4,9 @@ describe('Ten Pin Bowling', function(){
 		game = new Bowling;
 		expect(game.pins).toEqual(10);
 	});
+
+	it('Starts with ten frames', function(){
+		game = new Bowling;
+		expect(game.frames).toEqual(10);
+	});
 });
